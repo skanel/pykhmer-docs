df3263c5ee7995b18061413e6d716a03b34c4d3b
Soeng Kanel <Kanel.Soeng@amkcambodia.com>
2019-04-10 20:30:31 +0700

    update file 10-04-2019 training

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 ![PyKhmer](https://github.com/skanel/pykhmer-docs/blob/master/pykhmer.jpg?raw=true "pykhmer logo")
 
-The document use to train pytkhmer team about python programming language.
+This document is used for training pykhmer team to prepare experienced engineers to be productive python programmers. 
+Python is a programming language that lets you work quickly and integrate systems more effectively. 
+Thus, these lectures assumed the audience knew the concepts and showed how to use them in Python.
+ We found this an effective way of getting new engineers up to speed quickly. 
+ This is the written material that accompanied those lectures. 
+ We have found that these are useful in their own right.
+
+
--- ex_write_csv_find_max_sorting_list.py
+++ ex_write_csv_find_max_sorting_list.py
@@ -4,10 +4,10 @@ file = open("E:\\DOCUMENT\\Lecture\\pykhmer-docs\\customer.csv","w+")
 header = ["id", "firstname", "lastname", "age", "phone_number", "missed_call", "date"]
 
 data  = [
-            ["1","Sok", "Reaksmey", "29", "092222999", "19", "20190405"],
-            ["2","tep", "nimit", "39", "092111999", "9", "20190404"],
-            ["3","ket", "chan", "40", "012345666", "10", "20190404"],
-            ["4","pongsa", "metrey", "19", "092002999", "11", "20190405"]
+            ["1","Sok", "Reaksmey", 12, "092222999", "19", "20190405"],
+            ["2","tep", "nimit", 11, "092111999", "9", "20190404"],
+            ["3","ket", "chan", 9, "012345666", "10", "20190404"],
+            ["4","pongsa", "metrey", 10, "092002999", "11", "20190405"]
         ]
 
 
@@ -17,21 +17,29 @@ file.write(','.join(header)+"\n")
 who_is_max_missed_call = []
 maximum = 0
 date_looking = "20190404"
+data_dict = {}
 
+#Loop over data
 for item in data:
-    file.write(','.join(item)+"\n") 
+    file.write(','.join(str(item))+"\n") 
+    
+    #tranform list to dictionary
+    data_dict[item[3]] = item
     
     #3. find who is max of missed_call in the specific date
     if (int(item[5]) > int(maximum) and item[6] == date_looking):
         maximum = item[5]
+       
         who_is_max_missed_call= item
     else:
         maximum = maximum
     
-print(who_is_max_missed_call)    
+#print(who_is_max_missed_call)    
 
 #4. Sort customer list by age, and print it out
 
+#print(data_dict)
+result_sorted = [data_dict[key] for key in sorted(data_dict)]
+print(result_sorted)
 #do it yourself
-
 file.close() 
