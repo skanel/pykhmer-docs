4a87e3a08f77f4e44467ddba18e4912732353627
Soeng Kanel <Kanel.Soeng@amkcambodia.com>
2019-05-02 18:55:59 +0700

    add anwser for fx2

--- fx2.py
+++ fx2.py
@@ -33,3 +33,26 @@ else:
         print("សមីការមាន​ឬស មួយ ពីរ​ផ្សេងគ្នា")
         print("x1 = ", float((-b - sqrt(delta)) / (2 * a)))
 print("x2 = ", float((-b + sqrt(delta)) / (2 * a)))
+
+
+# WRITE IN FUNCITON WAY
+
+#coding:utf-8
+#Program by Kanel
+import math
+def quadratic(a,b,c):
+    delta = b*b - 4*a*c
+    if delta > 0 :
+        x1 = (-b + math.sqrt(delta))/(2*a)
+        x2 = (-b - math.sqrt(delta))/(2*a)
+        print "សមីការដឺក្រេទី 2 មាន​ឬសពីរ​ផ្សេងគ្នា"
+        print "x1= %3.2f x2= %3.2f " %(x1,x2)
+    elif delta ==0 :
+       print "សមីកាមាន​ឬស​ឌុប x1=x2= %3.2f" %(-b/2*a)
+    else :
+       print "សមីការគ្មាន​ឬស"  
+print "ដាក់បញ្ចូលតាម​លំដាប់លំដោយ មេគុណ a,b,c របស់​សមីការ​ដឹក្រេទី២ "	   
+a = float(raw_input('មេគុណ a:'))
+b = float(input('មេគុណ b:'))
+c = float(input('មេ c:'))
+quadratic(a,b,c)	
